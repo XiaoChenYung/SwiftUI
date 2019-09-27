91d6520bdca1e7a674d51f3e8193872c481ea208
杨晓晨 <xiaochenyung@gmail.com>
2019-09-27 18:04:02 +0800

    添加圆形 image

--- SwiftUI-P1/SwiftUI-P1/ContentView.swift
+++ SwiftUI-P1/SwiftUI-P1/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+		// 创建一个垂直容器, 左对齐
+		VStack(alignment: .leading) {
+			// 设置一个 font 为 title 的标题0
+			Text("Turtle Rock")
+				.font(.title)
+			// 创建一个水平容器
+			HStack {
+				// 设置一个 font 为 subheadline 的详情
+				Text("Joshua Tree National Park")
+					.font(.subheadline)
+				Spacer()
+				// 设置一个 font 为 subheadline 的详情
+				Text("California")
+					.font(.subheadline)
+			}
+		}
+			// 设置该 VStack 距离左右的边距是 10
+		.padding(EdgeInsets.init(top: 0, leading: 10, bottom: 0, trailing: 10))
     }
 }
 
