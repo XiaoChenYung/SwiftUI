665c24a3b275540ba9da7845c82a131a55c9e5eb
杨晓晨 <xiaochenyung@gmail.com>
2019-09-27 16:37:34 +0800

    init

---
+++ SwiftUI-P1/SwiftUI-P1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-P1
+//
+//  Created by wanba on 2019/9/27.
+//  Copyright © 2019 yangxiaochen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
